65fbfe4ed2305e29782750ead0428aab6c83252e
unknown <A5105198@DSG1303168.OCBCGROUP.OCBC.COM>
2014-06-23 13:52:29 +0800

    some progress on perf

--- research/src/simple_mc_loop.R
+++ research/src/simple_mc_loop.R
@@ -1,4 +1,10 @@
-NumberSimulation = 100
+library(compiler)
+#install.packages("lineprof")
+#library(lineprof)
+#install.packages("Rprof")
+#library(Rprof)
+
+NumberSimulation = 10000
 NumberOfTimesteps = 200
 maturityBucket = 1/12
 NumberOfYear = 2
@@ -22,13 +28,48 @@ for (j in seq(1,length(MaturityList))) {
 #init of random number
 dX_Master = rnorm(n = NumberSimulation * 3 * NumberOfTimesteps, mean = 0, sd = 1)
 
+
+
+testfunc = function(i,j,mat) {
+  if (j<ncol(mat)) {
+    result = mat[i-1,j] + quantity_1[j]*timestep + 
+      sum(quantity_2[j]*dX[i-1,1],quantity_3[j]*dX[i-1,2],quantity_4[j]*dX[i-1,3])*sqrt(timestep) +
+      ((mat[i-1,j+1]-mat[i-1,j])/(maturityBucket))*timestep 
+  }
+  else if (j == ncol(mat)) {
+    result = mat[i-1,j] + quantity_1[j]*timestep + 
+      sum(quantity_2[j]*dX[i-1,1],quantity_3[j]*dX[i-1,2],quantity_4[j]*dX[i-1,3])*sqrt(timestep) +
+      ((mat[i-1,j]-mat[i-1,j-1])/(maturityBucket))*timestep 
+  }
+  return(result)
+}
+testfunc.compiled = cmpfun(testfunc)
+
+
+testfunc2 = function(i,mat) {
+  vect = rep(NA,ncol(mat))
+  
+  for (j in seq(1,ncol(mat)-1)) {
+    vect[j] = mat[i-1,j] + quantity_1[j]*timestep + 
+      sum(quantity_2[j]*dX[i-1,1],quantity_3[j]*dX[i-1,2],quantity_4[j]*dX[i-1,3])*sqrt(timestep) +
+      ((mat[i-1,j+1]-mat[i-1,j])/(maturityBucket))*timestep 
+  }
+  
+  vect[ncol(mat)] = mat[i-1,j] + quantity_1[j]*timestep + 
+    sum(quantity_2[j]*dX[i-1,1],quantity_3[j]*dX[i-1,2],quantity_4[j]*dX[i-1,3])*sqrt(timestep) +
+    ((mat[i-1,j]-mat[i-1,j-1])/(maturityBucket))*timestep 
+
+  return(vect)
+}
+testfunc2.compiled = cmpfun(testfunc2)
+
+testfunc3 = function(mat){
+  return(sapply(seq(2,nrow(mat)),testfunc2.compiled,mat=mat))
+}
+testfunc3.compiled = cmpfun(testfunc3)
+
 timestamp = Sys.time()
 #monte carlo loop
-
-#testfunc = function(i,j) {
-#  
-#}
-
 
 for (k in seq(1,NumberSimulation)){
   if (k%%(NumberSimulation/20) == 0) cat((k/NumberSimulation)*100,"% ...\n")
@@ -39,26 +80,19 @@ for (k in seq(1,NumberSimulation)){
   dX = matrix(dX_Master[((k-1)* 3 * NumberOfTimesteps+1):(k * 3 * NumberOfTimesteps)],ncol=3,nrow=NumberOfTimesteps,byrow = TRUE) 
   
   #matrix population
-  for (i in seq(2,nrow(mat))) {  
-    for (j in seq(1,ncol(mat))) {
-      if (j<ncol(mat)) {
-        mat[i,j] = mat[i-1,j] + quantity_1[j]*timestep + 
-          sum(quantity_2[j]*dX[i-1,1],quantity_3[j]*dX[i-1,2],quantity_4[j]*dX[i-1,3])*sqrt(timestep) +
-          ((mat[i-1,j+1]-mat[i-1,j])/(maturityBucket))*timestep 
-      }
-      else if (j == ncol(mat)) {
-        mat[i,j] = mat[i-1,j] + quantity_1[j]*timestep + 
-          sum(quantity_2[j]*dX[i-1,1],quantity_3[j]*dX[i-1,2],quantity_4[j]*dX[i-1,3])*sqrt(timestep) +
-          ((mat[i-1,j]-mat[i-1,j-1])/(maturityBucket))*timestep 
-      }
-      
-    }
-  }
+  #for (i in seq(2,nrow(mat))) {  
+    #for (j in seq(1,ncol(mat))) {
+    #  mat[i,j] = testfunc.compiled(i,j,mat)       
+    #}
+    #mat[i,] = testfunc2.compiled(i,mat)
+  #}
   
-  
+  #mat = sapply(seq(2,nrow(mat)),testfunc2.compiled,mat=mat)
+  mat = testfunc3.compiled(mat)
   
   #perform calculation using data in matrix mat
   #...
 }
 cat("time to complete MC loop:",Sys.time()-timestamp, "\n")
+
 
