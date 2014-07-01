942d0a463166e18a971989e45184c27ba763bd9c
Bertrand <Bertrand.LeNezet@gmail.com>
2014-06-30 23:05:00 -0700

    issue pricing fixed

--- creditbasket/src/sampling_from_gaussian_copula.R
+++ creditbasket/src/sampling_from_gaussian_copula.R
@@ -8,11 +8,11 @@ spread_gaussian = BasketCDSPricing_GaussianCopula(CDS1_USD_XR_MARGINAL_CreditCur
 
 
 UniformCorrelationMatrix = function(rho,n) matrix(rho,nrow=n,ncol=n) + (1-rho)*diag(n)
-corrmat = UniformCorrelationMatrix(sqrt(0.3),5)
+corrmat = UniformCorrelationMatrix(sqrt(0.6),5)
 cc = BootstrapCreditCurve(c(new ("CreditDefaultSwap", maturity = 1, marketprice = 400.00),
-                       new ("CreditDefaultSwap", maturity = 2, marketprice = 420.00),
-                       new ("CreditDefaultSwap", maturity = 3, marketprice = 440.00),
-                       new ("CreditDefaultSwap", maturity = 4, marketprice = 460.00),
-                       new ("CreditDefaultSwap", maturity = 5, marketprice = 480.00)),0.40,YieldCurve)
+                       new ("CreditDefaultSwap", maturity = 2, marketprice = 400.00),
+                       new ("CreditDefaultSwap", maturity = 3, marketprice = 400.00),
+                       new ("CreditDefaultSwap", maturity = 4, marketprice = 400.00),
+                       new ("CreditDefaultSwap", maturity = 5, marketprice = 400.00)),0.40,YieldCurve)
 
-BasketCDSPricing_GaussianCopula(cc,cc,cc,cc,cc,corrmat,0.40,1,50000)
+BasketCDSPricing_GaussianCopula(cc,cc,cc,cc,cc,YieldCurve,corrmat,0.40,2,50000)
